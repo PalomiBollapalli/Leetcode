cb2a7ca4086af8d368afad5b7d4716b10851cab9
PalomiBollapalli <bollapallipalomi@gmail.com>
2026-08-10 11:59:44 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.85%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int flag=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==target){
+                    flag=1;
+                }
+            }
+        }
+        return flag==1;
+    }
+}
