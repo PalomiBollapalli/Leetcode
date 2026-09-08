a896e7025e476b5e947636544888417503ddcc5a
PalomiBollapalli <bollapallipalomi@gmail.com>
2026-09-08 12:44:16 +0530

    Time: 40 ms (67.12%), Space: 98.9 MB (58.93%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,40 +1,26 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-
-        HashSet<Integer> set = new HashSet<>();
-
-        long sum = 0;
-        long maxSum = 0;
-
-        int left = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-
-            // If duplicate exists, remove elements
-            // from the left until duplicate is gone
-            while (set.contains(nums[right])) {
+        HashSet<Integer> set=new HashSet<>();
+        long sum=0;
+        int left=0;
+        long maxSum=0;
+        for(int i=0;i<nums.length;i++){
+            while(set.contains(nums[i])){
                 set.remove(nums[left]);
-                sum -= nums[left];
+                sum=sum-nums[left];
                 left++;
             }
-
-            // Add current element
-            set.add(nums[right]);
-            sum += nums[right];
-
-            // Keep window size at most k
-            if (right - left + 1 > k) {
+            set.add(nums[i]);
+            sum=sum+nums[i];
+            if(i-left+1>k){
                 set.remove(nums[left]);
-                sum -= nums[left];
+                sum=sum-nums[left];
                 left++;
             }
-
-            // Check if window has exactly k distinct elements
-            if (right - left + 1 == k) {
-                maxSum = Math.max(maxSum, sum);
+            if(set.size()==k){
+                maxSum=Math.max(maxSum,sum);
             }
         }
-
         return maxSum;
     }
 }
