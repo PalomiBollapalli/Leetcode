6866b3900075f9cb1814147c7dcfd182d5621840
PalomiBollapalli <bollapallipalomi@gmail.com>
2026-09-16 14:16:48 +0530

    Time: 0 ms (100%), Space: 42.9 MB (44%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,13 +1,5 @@
 class Solution {
-    public String defangIPaddr(String ad) {
-        String ans="";
-        for(int i=0;i<ad.length();i++){
-            if(ad.charAt(i)=='.'){
-                ans=ans+"[.]";
-            }else{
-                ans=ans+ad.charAt(i);
-            }
-        }
-        return ans;
+    public String defangIPaddr(String address) {
+        return address.replace(".","[.]");
     }
 }
